32b29e4434936d572906c8fc433f4f133d51ce46
sigonasr2 <sigonasr2@gmail.com>
2019-06-13 04:04:45 +0900

    Add in game project. Setup bow asset. Working on Quaternion....

---
+++ SigDN/src/mygame/Main.java
@@ -0,0 +1,119 @@
+package mygame;
+
+import com.jme3.animation.AnimChannel;
+import com.jme3.animation.AnimControl;
+import com.jme3.app.SimpleApplication;
+import com.jme3.light.DirectionalLight;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
+import com.jme3.renderer.RenderManager;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.Spatial;
+import com.jme3.scene.shape.Box;
+import com.jme3.animation.*;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.math.FastMath;
+import com.jme3.math.Quaternion;
+import com.jme3.scene.Node;
+
+/**
+ * This is the Main Class of your Game. You should only do initialization here.
+ * Move your Logic into AppStates or Controls
+ * @author normenhansen
+ */
+public class Main extends SimpleApplication implements
+        AnimEventListener,ActionListener{
+    private AnimChannel channel;
+    private AnimControl control;
+    private Geometry geom;
+
+    public static void main(String[] args) {
+        Main app = new Main();
+        app.start();
+    }
+
+    @Override
+    public void simpleInitApp() {
+        
+        flyCam.setMoveSpeed(20f);
+        flyCam.setZoomSpeed(20f);
+        cam.setLocation(new Vector3f(6.4013605f, 7.488437f, 12.843031f));
+        cam.setRotation(new Quaternion(-0.060740203f, 0.93925786f, -0.2398315f, -0.2378785f));
+
+        // sunset light
+        DirectionalLight dl = new DirectionalLight();
+        dl.setDirection(new Vector3f(-0.1f, -0.7f, 1).normalizeLocal());
+        dl.setColor(new ColorRGBA(1f, 1f, 1f, 0.5f));
+        rootNode.addLight(dl);
+        DirectionalLight dl2 = new DirectionalLight();
+        dl2.setColor(new ColorRGBA(1f, 1f, 1f, 0.5f));
+        dl2.setDirection(new Vector3f(0.1f, -0.7f, -1).normalizeLocal());
+        rootNode.addLight(dl2);
+        Spatial Archer = (Spatial) assetManager.loadModel("Models/Archer_finalrig/Archer_finalrig.mesh.j3o");
+        float scale = 0.05f;
+        Archer.scale(scale, scale, scale);
+        
+        Spatial bow = (Spatial) assetManager.loadModel("Models/Smallbow_Mimiyala/Smallbow_Mimiyala.j3o");
+        
+        
+        control = Archer.getControl(AnimControl.class);
+        control.addListener(this);
+        channel = control.createChannel();
+        
+        for (String anim : control.getAnimationNames())
+            System.out.println(anim);
+        
+        channel.setAnim("Idle Animation");
+        geom = (Geometry)((Node)Archer).getChild(0);
+        SkeletonControl skeletonControl = Archer.getControl(SkeletonControl.class);
+        
+        Node n = skeletonControl.getAttachmentsNode("Torso");
+        bow.setLocalTranslation(5.7983f,2.0104f,12.5006f);
+        //bow.setLocalRotation(new Quaternion().fromAngleAxis(FastMath.PI*1f/4, new Vector3f(1,0,0)));
+        bow.setLocalRotation(new Quaternion().fromAngleAxis(FastMath.PI*1f/4, new Vector3f(1,0,0)).mult(
+        new Quaternion().fromAngleAxis(FastMath.PI*3f/2, new Vector3f(0,1,0))));
+        //n.set
+        n.attachChild(bow);
+        //n.setLocalTranslation(5.7983f,99.0104f,12.5006f);
+        for (Spatial s : n.getChildren()) {
+            System.out.println(s.getName());
+            //System.out.println(s.getLocalTransform());
+            System.out.println(s.getWorldTransform());
+        }
+        
+        rootNode.attachChild(Archer);
+        //rootNode.attachChild(bow);
+    }
+
+    @Override
+    public void simpleUpdate(float tpf) {
+        //TODO: add update code
+    }
+
+    @Override
+    public void simpleRender(RenderManager rm) {
+        //TODO: add render code
+    }
+
+    public void onAnimCycleDone(AnimControl control, AnimChannel channel, String animName) {
+        if (animName.equals("Dodge")){
+            channel.setAnim("stand", 0.50f);
+            channel.setLoopMode(LoopMode.DontLoop);
+            channel.setSpeed(1f);
+        }
+    }
+
+    public void onAnimChange(AnimControl control, AnimChannel channel, String animName) {
+    }
+
+    public void onAction(String binding, boolean value, float tpf) {
+        if (binding.equals("Attack") && value){
+            if (!channel.getAnimationName().equals("Dodge")){
+                channel.setAnim("Dodge", 0.50f);
+                channel.setLoopMode(LoopMode.Cycle);
+                channel.setSpeed(0.10f);
+            }
+        }
+    }
+}
